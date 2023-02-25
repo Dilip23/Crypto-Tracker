7ca09cc0405efeed1a4a1c0624d61c2194afdc1b
Dilip23 <dilipkrishna.adapaka@gmail.com>
2023-02-25 11:28:50 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <h1 className="text-center">App</h1>
+    </div>
+  )
+}
+
+export default App
